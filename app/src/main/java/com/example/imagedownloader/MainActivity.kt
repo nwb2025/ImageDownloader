c181a9f162f0e3f34c17e0254b72f1519a6ec177
Ertel Renat <nwb2025@gmail.com>
2021-05-12 13:31:14 +0500

    without glide dependencies

--- app/src/main/java/com/example/imagedownloader/MainActivity.kt
+++ app/src/main/java/com/example/imagedownloader/MainActivity.kt
@@ -1,24 +1,25 @@
 package com.example.imagedownloader
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-import com.bumptech.glide.Glide
+import androidx.appcompat.app.AppCompatActivity
+import java.net.MalformedURLException
+import java.net.URL
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var et_uri:EditText
     private lateinit var btn_upload:Button
     private lateinit var img_view:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
-
-
     }
 
     private fun initViews(){
@@ -26,14 +27,19 @@ class MainActivity : AppCompatActivity() {
         btn_upload = findViewById(R.id.btn_upload)
         img_view = findViewById(R.id.img_view)
         btn_upload.setOnClickListener {
-            if ( et_uri.text.toString() == "") et_uri.setError("This field can not be empty")
+            if ( et_uri.text.toString() == "") et_uri.error = "This field can not be empty"
             else{
-                Glide.with(this)
-                    .load(et_uri.text.toString())
-                    .centerCrop()
-                    .into(img_view)
+                val url = URL(et_uri.text.toString())
+                Thread(Runnable {
+                    val bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+                    img_view.post {
+                        img_view.setImageBitmap(bmp)
+                    }
+                }).start()
             }
-
         }
     }
 }
+
+
+
